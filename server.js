dcdd13a0508fe9849af5882edcfb962f61e1ec0a
Alfy Hushairi <alfy.hushairi@accedo.tv>
2023-06-12 10:55:34 +0100

    added /imagemock

--- server.js
+++ server.js
@@ -15,14 +15,29 @@ const openai = new OpenAIApi(configuration);
 const app = express();
 app.use(cors());
 app.use(express.json());
+const router = express.Router();
 
-app.get('/', async (req, res) => {
+app.use('/', router);
+
+router.get('/', async (req, res) => {
   res.status(200).send({
     message: "Hello from server"
   })
 });
 
-app.post('/', async (req, res) => {
+router.post("/imagemock", async (req, res) => {
+  try {
+    res.status(200).send({
+      image:
+        "https://oaidalleapiprodscus.blob.core.windows.net/private/org-LmA6NA4u0UQ2HUDkZzIhoHFR/user-EvjNfLp4XB6oXxIgz3Mg8QYm/img-VDe5JGaHZsYyOSjGYkg2LJmg.png?st=2023-06-12T08%3A17%3A04Z&se=2023-06-12T10%3A17%3A04Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-06-11T20%3A41%3A02Z&ske=2023-06-12T20%3A41%3A02Z&sks=b&skv=2021-08-06&sig=HndIUOc%2BfoB3HVLmPMzY%2Beq24TUCBGGYMtBDVMTadZg%3D",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
+
+router.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
